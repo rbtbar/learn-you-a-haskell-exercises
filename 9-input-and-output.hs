669122a2bc7e6f8580ac358f1087151c77d7635c
Robert Bar <robert@rbsoftware.pl>
2015-09-05 18:44:01 +0200

    Add solutions to chapter 9
    Add solutions to chapter 9

--- 9-input-and-output.hs
+++ 9-input-and-output.hs
@@ -3,11 +3,23 @@
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
-  
-main = undefined
+import System.Environment
+import System.Random
+import Data.List
+
+
+main :: IO ()
+main = do
+  (command:args) <- getArgs
+  if command == "-n"
+    then putStr $ unwords args
+    else putStrLn $ unwords (command:args)
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
  -}
 lottery :: StdGen -> [Int]
-lottery gen = undefined
+lottery = sort . take 6 . randomRs (1,49)
+
+-- Example:
+--lottery (mkStdGen 11)
